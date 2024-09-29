80352e89b57342d085ca5f4c188769a4885ebd3e
Admin Deltastateonline <joomla@deltastateonline.com>
2024-09-29 12:27:43 +1000

    Remove folder Created In error

--- src/ShippingNoticeTest/ShippingNoticeGetTests.cs
+++ src/ShippingNoticeTest/ShippingNoticeGetTests.cs
@@ -67,5 +67,51 @@ namespace ShippingNoticeTest
             // Assert
             Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
         }
+
+        [Fact]
+        public async Task GetShippingnNoticeById_ReturnsBadRequest_WhenShipmentIdIsNull()
+        {
+            // Arrange
+            var context = new Mock<FunctionContext>();
+            var request = new Mock<HttpRequestData>(context.Object);
+            var response = new Mock<HttpResponseData>(context.Object);
+
+            request.Setup(x => x.CreateResponse()).Returns(response.Object);
+            response.SetupProperty(x => x.StatusCode, HttpStatusCode.OK);
+            response.Setup(x => x.Headers).Returns(new HttpHeadersCollection());
+
+            // Act
+            var result = await _function.GetShippingnNoticeById(request.Object, null, context.Object);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+        [Fact]
+        public async Task GetShippingnNoticeById_ReturnsOkResponse_WithShippingNotice()
+        {
+            // Arrange
+            var context = new Mock<FunctionContext>();
+            var request = new Mock<HttpRequestData>(context.Object);
+            var response = new Mock<HttpResponseData>(context.Object);
+
+            var shippingNotice = 
+                new Deltastateonline.Models.ShippingNotice { ShipmentId = "S001", ExpectedArrival = DateTime.Now.AddDays(3), CarrierName = "FastShip", TrackingNumber = "TRACK001" }
+           ;
+
+
+            _mockShippingNoticeProvider.Setup(x => x.GetShippingNotice(It.IsAny<string>())).ReturnsAsync(shippingNotice);
+
+            request.Setup(x => x.CreateResponse()).Returns(response.Object);
+            response.SetupProperty(x => x.StatusCode, HttpStatusCode.OK);
+            response.Setup(x => x.Headers).Returns(new HttpHeadersCollection());
+            //response.Setup(x => x.WriteStringAsync(It.IsAny<string>())).Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _function.GetShippingnNoticeById(request.Object, "validId", context.Object);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+            _mockShippingNoticeProvider.Verify(x => x.GetShippingNotice("validId"), Times.Once);
+        }
     }
 }
